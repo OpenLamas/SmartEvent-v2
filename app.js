45e15291bd4a3f6d47fa90ea5af2b5bad0be5639
Julien Brun <jb007270@hotmail.fr>
2012-08-21 14:57:37 +0200

    Correction mineur

--- app.js
+++ app.js
@@ -132,7 +132,7 @@ var template = twig({
 app.get('/', function(req, res){
   var data = { 'site_root': '/', 'cur_user': {'right': 'ADMIN', 'login': 'jeanPo', 'nomutilisateur': 'Polate', 'prenomutilisateur': 'Jean'}};
  return res.send(twig({ref: "home"}).render(data));
-};
+});
 // Launch server
 
 app.listen(8080);
