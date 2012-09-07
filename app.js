ddec835116b0fa05c3ee867f16282bdef25358fb
Julien Brun <jb007270@hotmail.fr>
2012-09-07 20:43:48 +0200

    Debug mineur

--- app.js
+++ app.js
@@ -12,16 +12,14 @@ clientRed.on("error", function (err){
   console.log("Error : " + err);
 });
 
-app.configure(function () {
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(application_root + '/client'));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
 
 app.get("/api/event/:id", function(req, res){
-  clientRed.hgetall(req.params.id, function (err, reply){
+  clientRed.hgetall("event:"+req.params.id, function (err, reply){
     if(!err){
       console.log("Event "+req.params.id+" requested");
       res.send(reply);
@@ -34,26 +32,28 @@ app.get("/api/event/:id", function(req, res){
 });
 
 app.put("/api/event/:id", function(req, res){
-  clientRed.HMset("event:"+req.params.id, req.body, redis.print);
+  clientRed.hmset("event:"+req.params.id, req.body, redis.print);
   res.send(200);
 });
 
 app.delete("/api/event/:id", function (req, res){
-  clientRed.del("event:"+req.params.id, redis.print)
-})
+  clientRed.del("event:"+req.params.id, redis.prinnt);
+  res.send(200);
+});
 
 app.post("/api/events", function(req, res){
   clientRed.get("events:nextId", function (err, id){ // On récup l'id de notre nouvel event
-  if(!err){
-    console.log("New event ! id:"+id); 
-    clientRed.incr("events:nextId", redis.print); // On incrémante pour le suivant
-    clientRed.HMset("event:"+id, req.body, redis.print);
-    res.send('{ id : '+id+'}');
-  }
-  else{
-    console.log("Error : "+ err);
-    res.send(500);
-  }
+    if(!err){
+      console.log("New event ! id:"+id); 
+      clientRed.incr("events:nextId", redis.print); // On incrémante pour le suivant
+      clientRed.hmset("event:"+id, req.body, redis.print);
+      res.send(201, '{ id : '+id+'}');
+    }
+    else{
+      console.log("Error : "+ err);
+      res.send(500);
+    }
+  });
 });
 
 app.get('/api/events', function (req, res){
