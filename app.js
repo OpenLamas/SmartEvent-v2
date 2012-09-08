523e62b251d3ed3fca6d3719e498031a192e56f0
Julien Brun <jb007270@hotmail.fr>
2012-09-08 15:36:17 +0200

    ajout get events et list des events

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ app.put("/api/event/:id", function(req, res){
 });
 
 app.delete("/api/event/:id", function (req, res){
-  clientRed.del("event:"+req.params.id, redis.prinnt);
+  clientRed.del("event:"+req.params.id, redis.print); // On supprime l'event
+  clientRed.srem("global:events", req.params.id; redis.print) // On l'enlève du set
   res.send(200);
 });
 
@@ -46,7 +47,8 @@ app.post("/api/events", function(req, res){
     if(!err){
       console.log("New event ! id:"+id); 
       clientRed.incr("events:nextId", redis.print); // On incrémante pour le suivant
-      clientRed.hmset("event:"+id, req.body, redis.print);
+      clientRed.hmset("event:"+id, req.body, redis.print); // on crée le hash event:id
+      clientRed.sadd("global:events", id, redis.print); // on ajoute l'id au set
       res.send(201, '{ id : '+id+'}');
     }
     else{
@@ -57,10 +59,22 @@ app.post("/api/events", function(req, res){
 });
 
 app.get('/api/events', function (req, res){
-  clientRed.get("events:nextId", function (err, id){
+  clientRed.smembers("global:events", function (err, reply){
     if(!err){
-      console.log("Reply :"+ id);
-      res.send('id : ' + id);
+      console.log("Reply :"+ reply);
+      var listEvents = new Array();
+      for(id in reply){
+        clientRed.get("event:"+id, function (err, event){
+          if(!err){
+            listEvents.push(event);
+          }
+          else{
+            console.log("Error : "+err);
+            res.send(500);
+          }
+        });
+      }
+      res.send(listEvents);
     }
     else{
       console.log("Error : "+ id);
