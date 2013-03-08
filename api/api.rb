75c2716a36e85ad2727c62c9378069696dc6a3cb
Maxime Mouchet <mouchet.max@gmail.com>
2013-03-08 17:37:40 +0100

    Suite API

--- api/api.rb
+++ api/api.rb
@@ -3,9 +3,12 @@ require 'grape'
 module SmartEvent
     class API < Grape::API
 
+      version 'v1', :using => :header, :vendor => 'smartevent'
+      format :json
+
       resource :sessions do
 
-        desc 'Return all the sessions.'
+        desc 'Return all sessions.'
         get do
         end
 
@@ -42,6 +45,94 @@ module SmartEvent
         desc 'Delete a session.'
         params do
           requires :id, :type => Integer, :desc => 'Session id.'
+        end
+        delete ':id' do
+        end
+
+      end
+
+      resource :events do
+
+        desc 'Return all events.'
+        get do
+        end
+
+        desc 'Return one event.'
+        params do
+          requires :id, :type => Integer, :desc => 'Event id.'
+        end
+        get ':id' do
+        end
+
+        desc 'Create an event.'
+        params do
+          requires :name, :type => String, :desc => 'Event name.'
+          requires :desc, :type => String, :desc => 'Event description.'
+          requires :location, :type => String, :desc => 'Location of the event.'
+          requires :starttime, :type => DateTime, :desc => 'Start date of the event.'
+          requires :endtime,   :type => DateTime, :desc => 'End date of the event.'
+        end
+        post do
+        end
+
+        desc 'Update an event.'
+        params do
+          requires :id, :type => Integer, :desc => 'Event id.'
+          requires :name, :type => String, :desc => 'Event name.'
+          requires :desc, :type => String, :desc => 'Event description.'
+          requires :location, :type => String, :desc => 'Location of the event.'
+          requires :starttime, :type => DateTime, :desc => 'Start date of the event.'
+          requires :endtime,   :type => DateTime, :desc => 'End date of the event.'
+        end
+        put ':id' do
+        end
+
+        desc 'Delete an event.'
+        params do
+          requires :id, :type => Integer, :desc => 'Event id.'
+        end
+        delete ':id' do
+        end
+
+      end
+
+      resource :users do
+
+        desc 'Get all users.'
+        get do
+        end
+
+        desc 'Get one user.'
+        params do
+          requires :id, :type => Integer, :desc => 'User id.'
+        end
+        get ':id' do
+        end
+
+        desc 'Create an user.'
+        params do
+          requires :firstname, :type => String, :desc => 'User firstname.'
+          requires :lastname,  :type => String, :desc => 'User lastname.'
+          requires :email, :type => String, :desc => 'User email.'
+          requires :role, :type => String, :desc => 'User role.'
+        end
+        post do
+        end
+
+        desc 'Update an user.'
+        params do
+          requires :id, :type => Integer, :desc => 'User id.'
+          requires :firstname, :type => String, :desc => 'User firstname.'
+          requires :lastname,  :type => String, :desc => 'User lastname.'
+          requires :email, :type => String, :desc => 'User email.'
+          requires :role,  :type => String, :desc => 'User role.'
+        end
+        put ':id' do
+        end
+
+        desc 'Delete an user.'
+        params do
+          requires :id, :type => Integer, :desc => 'User id.'
         end
         delete ':id' do
         end
