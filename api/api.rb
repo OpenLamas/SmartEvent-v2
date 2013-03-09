f020ac64b11a50ca328acb8cad4c20b4bc61bcd4
Maxime Mouchet <mouchet.max@gmail.com>
2013-03-09 16:37:28 +0100

    Ajout Swagger-UI

--- api/api.rb
+++ api/api.rb
@@ -1,5 +1,6 @@
-require 'grape'
 require 'mongo'
+require 'grape'
+require 'grape-swagger'
 require './api_v1/sessions'
 require './api_v1/events'
 require './api_v1/users'
@@ -14,6 +15,8 @@ module SmartEvent
       mount SmartEvent::APIv1::Sessions
       mount SmartEvent::APIv1::Events
       mount SmartEvent::APIv1::Users
+
+      add_swagger_documentation({:api_version => 'v1', :base_path => 'http://127.0.0.1:9292/api/v1/'})
 
     end
 end
