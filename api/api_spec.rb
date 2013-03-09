0949ee4b0f968811eaa66324957a77a572d20937
Maxime Mouchet <mouchet.max@gmail.com>
2013-03-09 10:36:07 +0100

    Correction gitingore

--- api/api_spec.rb
+++ api/api_spec.rb
@@ -12,7 +12,7 @@ describe SmartEvent::API do
     describe 'POST /api/v1/users' do
       it 'should insert an user and return its id' do
         post '/api/v1/users', {'firstname' => 'Yvan', 'lastname' => 'Deschapeaux', 'email' => 'yvan.deschapeaux@chut.com', 'role' => 'admin'}
-        @user_id = last_response.body
+        puts @user_id = last_response.body
         last_response.status.should == 201
       end
     end
@@ -27,6 +27,7 @@ describe SmartEvent::API do
     describe 'GET /api/v1/users/@user_id' do
       it 'should return 1 user' do
         get "/api/v1/users/#{@user_id}"
+        puts @user_id
         result = JSON.parse(last_response.body)
         expect(result.length).to eq(1)
         expect(result['lastname']).to eq('Deschapeaux')
